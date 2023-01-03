6b641fa4eb40c655a3fc55103a2b97dd36cd1648
Clériot <louis.cleriot@telecomnancy.eu>
2023-01-03 10:43:36 +0100

    [~] Tag

---
+++ src/main/java/eu/telecomnancy/model/Tag.java
@@ -0,0 +1,36 @@
+package eu.telecomnancy.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tag {
+    private static final List<Tag> VALUES = new ArrayList<>();
+
+    private final String name;
+
+    private Tag(String name) {
+        this.name = name;
+        VALUES.add(this);
+    }
+
+    public static Tag createTag(String name) {
+        return new Tag(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Tag[] values() {
+        return VALUES.toArray(new Tag[0]);
+    }
+
+    public static Tag valueOf(String name) {
+        for (Tag t : VALUES) {
+            if (t.getName().equals(name)) {
+                return t;
+            }
+        }
+        throw new IllegalArgumentException("No such tag: " + name);
+    }
+}

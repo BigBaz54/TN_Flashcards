33e7b926300758c52bf5704177061e0f9d73f0f8
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2023-01-03 21:10:25 +0100

    [~] fix parce que DeckCells a changé

--- src/main/java/eu/telecomnancy/buildCardStrategy/BuildCardStrategyClassic.java
+++ src/main/java/eu/telecomnancy/buildCardStrategy/BuildCardStrategyClassic.java
@@ -5,6 +5,6 @@ import eu.telecomnancy.view.DeckCell;
 public class BuildCardStrategyClassic implements BuildCardStrategy {
     public DeckCell buildCard() {
         // TODO
-        return new DeckCell();
+        return null;
     }
 }

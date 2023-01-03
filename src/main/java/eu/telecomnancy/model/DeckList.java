48dc69f141472c6650d9061f6e64d608065026e3
Clériot <louis.cleriot@telecomnancy.eu>
2023-01-03 11:27:37 +0100

    [+] decklists et modif card

--- src/main/java/eu/telecomnancy/model/DeckList.java
+++ src/main/java/eu/telecomnancy/model/DeckList.java
@@ -1,4 +1,28 @@
 package eu.telecomnancy.model;
 
+import java.util.ArrayList;
+
 public class DeckList {
+    private static final DeckList INSTANCE = new DeckList();
+    private final ArrayList<Deck> decks;
+
+    private DeckList() {
+        decks = new ArrayList<>();
+    }
+
+    public static DeckList getInstance() {
+        return INSTANCE;
+    }
+
+    public void addDeck(Deck deck) {
+        decks.add(deck);
+    }
+
+    public void removeDeck(Deck deck) {
+        decks.remove(deck);
+    }
+
+    public Deck[] getDecks() {
+        return decks.toArray(new Deck[0]);
+    }
 }

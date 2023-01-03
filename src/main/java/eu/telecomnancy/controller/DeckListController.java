b3817facde65b95a12fbd43c429ca4fb0f442450
thibault-cne <thibault.chene23@gmail.com>
2023-01-03 11:34:14 +0100

    [+] add controller

---
+++ src/main/java/eu/telecomnancy/controller/DeckListController.java
@@ -0,0 +1,19 @@
+package eu.telecomnancy.controller;
+
+public class DeckListController implements Controller {
+    public void createDeck(String name) {
+        // TODO:
+    }
+
+    public void deleteDeck(int i) {
+        // TODO:
+    }
+
+    public void exportDeck(int i) {
+        // TODO:
+    }
+
+    public void importDeck() {
+        // TODO:
+    }
+}

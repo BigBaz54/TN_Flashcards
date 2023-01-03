79ba78b8c9ce6fa7a1628c85b5439d89d63107fc
Clériot <louis.cleriot@telecomnancy.eu>
2023-01-03 19:50:50 +0100

    [+]debut Stat

---
+++ src/main/java/eu/telecomnancy/model/StatCard.java
@@ -0,0 +1,78 @@
+package eu.telecomnancy.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Timer;
+
+public class StatCard {
+    private int nbTimesSeen;
+    private int nbTimesCorrect;
+    private int nbTimesWrong;
+    private ArrayList<Long> timesSpent;
+
+    /*  How to record time
+        Long startTime = System.currentTimeMillis();
+        Do something
+        long endTime = System.currentTimeMillis();
+        long duration = (endTime - startTime);
+     */
+
+    public StatCard() {
+        this.nbTimesSeen = 0;
+        this.nbTimesCorrect = 0;
+        this.nbTimesWrong = 0;
+        this.timesSpent = new ArrayList<>();
+        this.timesSpent.add(0L);
+    }
+
+    public int getNbTimesSeen() {
+        return nbTimesSeen;
+    }
+
+    public void setNbTimesSeen(int nbTimesSeen) {
+        this.nbTimesSeen = nbTimesSeen;
+    }
+
+    public int getNbTimesCorrect() {
+        return nbTimesCorrect;
+    }
+
+    public void setNbTimesCorrect(int nbTimesCorrect) {
+        this.nbTimesCorrect = nbTimesCorrect;
+    }
+
+    public int getNbTimesWrong() {
+        return nbTimesWrong;
+    }
+
+    public void setNbTimesWrong(int nbTimesWrong) {
+        this.nbTimesWrong = nbTimesWrong;
+    }
+
+    public ArrayList<Long> getTimesSpent() {
+        return timesSpent;
+    }
+
+    public Long getTimesSpentLast() {
+        return timesSpent.get(timesSpent.size()-1);
+    }
+    public Long getTimesSpentTotal() {
+        Long total = 0L;
+        for (Long time : timesSpent) {
+            total += time;
+        }
+        return total;
+    }
+
+    public Long getTimesSpentAverage() {
+        Long total = 0L;
+        for (Long time : timesSpent) {
+            total += time;
+        }
+        return total/timesSpent.size();
+    }
+
+    public void setTimesSpent(ArrayList<Long> times) {
+        this.timesSpent = times;
+    }
+}
---
+++ src/main/java/eu/telecomnancy/model/StatDeckList.java
@@ -0,0 +1,114 @@
+package eu.telecomnancy.model;
+
+import java.util.ArrayList;
+import java.util.Timer;
+
+public class StatDeckList {
+    private int nbDecks;
+    private int nbCards;
+    private int nbTimesCorrect;
+    private int nbTimesWrong;
+    private int nbCardsSeen;
+    private ArrayList<Float> pourcentageTimesSpent;
+    private ArrayList<StatDeck> decks;
+
+    public StatDeckList() {
+        this.nbDecks = 0;
+        this.nbCards = 0;
+        this.nbTimesCorrect = 0;
+        this.nbTimesWrong = 0;
+        this.nbCardsSeen = 0;
+        this.pourcentageTimesSpent = new ArrayList<>();
+        this.decks = new ArrayList<>();
+    }
+
+    public void addDeck(StatDeck deck) {
+        this.decks.add(deck);
+        this.nbDecks++;
+        this.nbCards += deck.getNbCards();
+        this.nbTimesCorrect += deck.getNbTimesCorrect();
+        this.nbTimesWrong += deck.getNbTimesWrong();
+        this.nbCardsSeen += deck.getNbCardsSeen();
+        setPourcentageTimesSpent(decks);
+    }
+
+    public void removeDeck(StatDeck deck) {
+        this.decks.remove(deck);
+        this.nbDecks--;
+        this.nbCards -= deck.getNbCards();
+        this.nbTimesCorrect -= deck.getNbTimesCorrect();
+        this.nbTimesWrong -= deck.getNbTimesWrong();
+        this.nbCardsSeen -= deck.getNbCardsSeen();
+        setPourcentageTimesSpent(decks);
+    }
+
+
+
+    public int getNbDecks() {
+        return nbDecks;
+    }
+
+    public void setNbDecks(int nbDecks) {
+        this.nbDecks = nbDecks;
+    }
+
+    public int getNbCards() {
+        return nbCards;
+    }
+
+    public void setNbCards(int nbCards) {
+        this.nbCards = nbCards;
+    }
+
+    public int getNbTimesCorrect() {
+        return nbTimesCorrect;
+    }
+
+    public void setNbTimesCorrect(int nbTimesCorrect) {
+        this.nbTimesCorrect = nbTimesCorrect;
+    }
+
+    public int getNbTimesWrong() {
+        return nbTimesWrong;
+    }
+
+    public void setNbTimesWrong(int nbTimesWrong) {
+        this.nbTimesWrong = nbTimesWrong;
+    }
+
+    public int getNbCardsSeen() {
+        return nbCardsSeen;
+    }
+
+    public void setNbCardsSeen(int nbCardsSeen) {
+        this.nbCardsSeen = nbCardsSeen;
+    }
+
+    public ArrayList<Float> getPourcentageTimesSpent() {
+        return pourcentageTimesSpent;
+    }
+
+    public void setPourcentageTimesSpent(ArrayList<StatDeck> decks) {
+        ArrayList<Long> timeEachDeck = new ArrayList<>();
+        for (StatDeck deck : decks) {
+            timeEachDeck.add(deck.getTimesSpent());
+        }
+        float totalTime = 0;
+        for (Long time : timeEachDeck) {
+            totalTime += time;
+        }
+        for (Long time : timeEachDeck) {
+            this.pourcentageTimesSpent.add(time/totalTime);
+        }
+    }
+
+    public ArrayList<StatDeck> getDecks() {
+        return decks;
+    }
+
+    public void setDecks(ArrayList<StatDeck> decks) {
+        this.decks = decks;
+    }
+
+
+}

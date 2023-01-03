8d4edf70e43da8927857c534a9bbf8489b5b715f
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2023-01-03 21:00:22 +0100

    [+] deckController

--- src/main/java/eu/telecomnancy/controller/DeckController.java
+++ src/main/java/eu/telecomnancy/controller/DeckController.java
@@ -1,5 +1,30 @@
 package eu.telecomnancy.controller;
 
-public class DeckController {
+import eu.telecomnancy.model.DeckModel;
+import eu.telecomnancy.view.DeckView;
 
+public class DeckController {
+    private DeckModel deckModel;
+    private DeckView deckView;
+
+    public DeckController(DeckModel deckModel, DeckView deckView) {
+        this.deckModel = deckModel;
+        this.deckView = deckView;
+    }
+
+    public void setName(String name) {
+        deckModel.setName(name);
+    }
+
+    public void setDescription(String description) {
+        deckModel.setDescription(description);
+    }
+
+    public void addCard(String question, String answer) {
+        deckModel.addCard(question, answer);
+    }
+
+    public void removeCard(int i) {
+        deckModel.removeCard(i);
+    }
 }

407689852291812f4026bb54d7f5ec186f2773e9
thibault-cne <thibault.chene23@gmail.com>
2023-01-04 10:10:13 +0100

    [+] add things to gitignore and add test folder

---
+++ src/test/java/eu/telecomnancy/io/JsonParserTest.java
@@ -0,0 +1,11 @@
+package eu.telecomnancy.io;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class JsonParserTest {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}

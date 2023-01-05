b616302e91a672095190ad02610ffe488fc0032e
Clériot <louis.cleriot@telecomnancy.eu>
2023-01-05 14:08:18 +0100

    [+] l'app permet de générer des questions et leur réponsees

--- src/main/java/eu/telecomnancy/view/PopUpCardView.java
+++ src/main/java/eu/telecomnancy/view/PopUpCardView.java
@@ -1,6 +1,8 @@
 package eu.telecomnancy.view;
 
+import eu.telecomnancy.GenerateQuestion;
 import eu.telecomnancy.GenerateResponse;
+import eu.telecomnancy.model.CardModel;
 import eu.telecomnancy.model.DeckModel;
 import eu.telecomnancy.controller.DeckController;
 import eu.telecomnancy.model.DeckListModel;
@@ -13,6 +15,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class PopUpCardView extends DeckObserver {
 
@@ -20,8 +23,9 @@ public class PopUpCardView extends DeckObserver {
     private TextField questionEdit;
     @FXML
     private TextField answerEdit;
+    @FXML
+    private TextField nbCardEdit;
 
-    private DeckModel deckModel;
     private DeckController deckController;
 
 
@@ -35,6 +39,18 @@ public class PopUpCardView extends DeckObserver {
         GenerateResponse generateResponse = new GenerateResponse();
         String response = generateResponse.getResponse(question);
         answerEdit.setText(response);
+    }
+    @FXML
+    public void generateCard(ActionEvent actionEvent) throws IOException {
+        String nbCard = nbCardEdit.getText();
+        int nbCardInt = Integer.parseInt(nbCard);
+        GenerateQuestion generateQuestion = new GenerateQuestion();
+        ArrayList<CardModel> newCards= generateQuestion.generateQuestion(deckModel,nbCardInt);
+        for (CardModel card : newCards){
+            deckController.addCard(card.getQuestion(),card.getAnswer());
+        }
+        Stage stage = (Stage) questionEdit.getScene().getWindow();
+        stage.close();
     }
     @FXML
     public void createCard(ActionEvent event){

62ab49fde758753cb3788b742cc6abd746df6995
thibault-cne <thibault.chene23@gmail.com>
2023-01-05 20:02:56 +0100

    [+] add javafx media

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,16 +2,18 @@ module eu.telecomnancy {
     requires transitive javafx.controls;
     requires transitive javafx.graphics;
     requires javafx.fxml;
+    requires javafx.media;
     requires transitive com.google.gson;
     requires org.apache.httpcomponents.httpcore;
     requires org.apache.httpcomponents.httpclient;
-    // requires java.sql;
+    requires java.sql;
     requires json;
 
     opens eu.telecomnancy to javafx.controls, javafx.fxml, com.google.gson;
     opens eu.telecomnancy.view to javafx.fxml;
     opens eu.telecomnancy.io.json to com.google.gson;
     opens eu.telecomnancy.io.file to com.google.gson;
+    opens eu.telecomnancy.io.sql to java.sql;
     opens eu.telecomnancy.io.adapter to com.google.gson;
     opens eu.telecomnancy.buildCardStrategy to javafx.fxml;
 

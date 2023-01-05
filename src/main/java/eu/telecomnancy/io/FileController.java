148910a16832c7a4bd666225c1ceaf155b1cb0be
thibault-cne <thibault.chene23@gmail.com>
2023-01-05 11:51:45 +0100

    [+] add method to the File controller to use FileExporter and FileImporter directly

--- src/main/java/eu/telecomnancy/io/FileController.java
+++ src/main/java/eu/telecomnancy/io/FileController.java
@@ -1,5 +1,6 @@
 package eu.telecomnancy.io;
 
+import java.io.File;
 import eu.telecomnancy.controller.DeckListController;
 import eu.telecomnancy.model.DeckModel;
 import eu.telecomnancy.model.Media;
@@ -8,12 +9,16 @@ public class FileController {
     private FileLoader fileLoader;
     private FileSaver fileSaver;
     private FileWriter fileWriter;
+    private FileImporter fileImporter;
+    private FileExporter fileExporter;
 
     public FileController(FileReader<DeckModel> fileReader,
             FileWriter fileWriter) {
         this.fileLoader = new FileLoader(fileReader);
         this.fileSaver = new FileSaver(fileWriter);
         this.fileWriter = fileWriter;
+        this.fileImporter = new FileImporter();
+        this.fileExporter = new FileExporter();
     }
 
     public void setDeckListController(DeckListController deckListController) {
@@ -35,6 +40,22 @@ public class FileController {
     public void saveMedia(Media media) {
         try {
             fileWriter.writeMedia(media);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void importDeck(File file) {
+        try {
+            fileImporter.imports(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void exportDeck(DeckModel deckModel) {
+        try {
+            fileExporter.export(deckModel);
         } catch (Exception e) {
             e.printStackTrace();
         }
